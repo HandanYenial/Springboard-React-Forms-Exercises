3aad1456310a3f8ffcf991fc6d00caa75fea7009
Your Name <you@example.com>
2022-07-29 14:49:00 -0400

    newtodoform.test

--- src/NewTodoForm.test.js
+++ src/NewTodoForm.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 
 it("renders without crashing", function () {
-  render(<Todo />);
+  render(<NewTodoForm />);
 });
 
 it("matches snapshot", function () {
-  const { asFragment } = render(<Todo />);
+  const { asFragment } = render(<NewTodoForm />);
   expect(asFragment()).toMatchSnapshot();
 });
