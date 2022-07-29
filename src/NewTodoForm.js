97fe5e115405997e72834424346d3d03a0f58666
Your Name <you@example.com>
2022-07-29 14:13:23 -0400

    newtodoform

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,8 +1,40 @@
 import React from 'react'
+import { useState } from "react"
 
-function NewTodoForm() {
+const NewTodoForm({ addTodo }) => {
+    const initialTodo = { 
+        text:"" , 
+        completed: false
+    }
+
+    const[todo , setTodo] = useState(initialTodo);
+    
+    const handleChange =(e) => {
+        const{ name, value } = e.target;
+        setTodo(todo => ({...todo, [name]: value}));
+    }
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        addItem({...todo });
+        setTodo(initialTodo);
+    }
+
   return (
-    <div>NewTodoForm</div>
+    <div>
+        <form onSubmit ={handleSubmit}>
+            <label htmlFor="todo"> Todo </label>
+            <input
+                  id = "todo"
+                  type = "text"
+                  name="name"
+                  placeholder = "Todo"
+                  value = {todo.name}
+                  onChange = {handleChange}
+            />
+            <button type="submit"> Add Todo </button>
+        </form>
+    </div>
   )
 }
 
